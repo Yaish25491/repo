e4cd3d15534e14e6456a1442e69b1b736612afe1
Yaish25491 <55248011+Yaish25491@users.noreply.github.com>
2021-11-05 10:33:29 +0200

    updating customer and store classes

--- BuyACar/Customer.py
+++ BuyACar/Customer.py
@@ -1,7 +1,23 @@
 class Customer:
-   customer_id = None
-   name = None
-   address = None
-   phone_number = 
-   gender
+
+   def __init__(self, params):
+      self.customer_id = params[0]
+      self.name = params[1]
+      self.address = params[2]
+      self.phone_number = params[3]
+      self.gender = params[4]
+
+    def __str__(self):
+        return "{}, {}, {}, {}, {}".format(self.customer_id, self.name, self.address, self.phone_number,
+                                           self.gender)
+
+    def __repr__(self):
+        return self.__str__
+
+    def print_me(self):
+        print("customer_id:    {}".format(self.customer_id))
+        print("name:           {}".format(self.name))
+        print("manufacturer:   {}".format(self.address))
+        print("phone_number:   {}".format(self.phone_number))
+        print("gender:         {}".format(self.gender))
 
